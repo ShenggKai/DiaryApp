f48a78efe27c9e73ee779af84f47c0de43f03327
Sheng Kai <maimemdada@gmail.com>
2023-01-04 19:40:09 +0700

    add menu panel

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnMenu = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // pnMenu
+            // 
+            this.pnMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnMenu.Name = "pnMenu";
+            this.pnMenu.Size = new System.Drawing.Size(240, 565);
+            this.pnMenu.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(972, 565);
+            this.Controls.Add(this.pnMenu);
             this.Name = "MainForm";
             this.Text = "DiaryApp";
             this.ResumeLayout(false);
@@ -42,5 +52,7 @@
         }
 
         #endregion
+
+        private Panel pnMenu;
     }
 }
