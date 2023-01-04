173d4d33f9421b2156d4bbb511c85eb8daddd7da
Sheng Kai <maimemdada@gmail.com>
2023-01-04 23:45:42 +0700

    add tab page

--- Source/LoginForm.Designer.cs
+++ Source/LoginForm.Designer.cs
@@ -30,17 +30,22 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.pnImage = new System.Windows.Forms.Panel();
+            this.pLock = new System.Windows.Forms.PictureBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.uiLine2 = new Sunny.UI.UILine();
+            this.pUser = new System.Windows.Forms.PictureBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.uiLine1 = new Sunny.UI.UILine();
             this.pClose = new System.Windows.Forms.PictureBox();
             this.lbDiaryApp = new System.Windows.Forms.Label();
-            this.uiLine1 = new Sunny.UI.UILine();
-            this.txtUserName = new System.Windows.Forms.TextBox();
-            this.pUser = new System.Windows.Forms.PictureBox();
-            this.uiLine2 = new Sunny.UI.UILine();
-            this.txtPass = new System.Windows.Forms.TextBox();
-            this.pLock = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pClose)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pUser)).BeginInit();
+            this.uiTabControl1 = new Sunny.UI.UITabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.pLock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pClose)).BeginInit();
+            this.uiTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnImage
@@ -52,6 +57,82 @@
             this.pnImage.Name = "pnImage";
             this.pnImage.Size = new System.Drawing.Size(426, 569);
             this.pnImage.TabIndex = 0;
+            // 
+            // pLock
+            // 
+            this.pLock.BackColor = System.Drawing.Color.Transparent;
+            this.pLock.Image = global::DiaryApp.Properties.Resources._lock;
+            this.pLock.Location = new System.Drawing.Point(91, 200);
+            this.pLock.Name = "pLock";
+            this.pLock.Size = new System.Drawing.Size(40, 40);
+            this.pLock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pLock.TabIndex = 5;
+            this.pLock.TabStop = false;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BackColor = System.Drawing.SystemColors.Control;
+            this.txtUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUserName.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUserName.Location = new System.Drawing.Point(151, 117);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(306, 29);
+            this.txtUserName.TabIndex = 4;
+            // 
+            // uiLine2
+            // 
+            this.uiLine2.FillColor = System.Drawing.Color.Transparent;
+            this.uiLine2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.uiLine2.LineCapSize = 10;
+            this.uiLine2.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
+            this.uiLine2.LineColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
+            this.uiLine2.LineDashStyle = Sunny.UI.UILineDashStyle.Solid;
+            this.uiLine2.LineSize = 2;
+            this.uiLine2.Location = new System.Drawing.Point(151, 240);
+            this.uiLine2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiLine2.Name = "uiLine2";
+            this.uiLine2.Size = new System.Drawing.Size(306, 10);
+            this.uiLine2.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLine2.TabIndex = 3;
+            this.uiLine2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // pUser
+            // 
+            this.pUser.BackColor = System.Drawing.Color.Transparent;
+            this.pUser.Image = global::DiaryApp.Properties.Resources.user_green;
+            this.pUser.Location = new System.Drawing.Point(91, 108);
+            this.pUser.Name = "pUser";
+            this.pUser.Size = new System.Drawing.Size(40, 40);
+            this.pUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pUser.TabIndex = 5;
+            this.pUser.TabStop = false;
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.SystemColors.Control;
+            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPass.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtPass.Location = new System.Drawing.Point(151, 209);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(306, 29);
+            this.txtPass.TabIndex = 4;
+            // 
+            // uiLine1
+            // 
+            this.uiLine1.FillColor = System.Drawing.Color.Transparent;
+            this.uiLine1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.uiLine1.LineCapSize = 10;
+            this.uiLine1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
+            this.uiLine1.LineColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
+            this.uiLine1.LineDashStyle = Sunny.UI.UILineDashStyle.Solid;
+            this.uiLine1.LineSize = 2;
+            this.uiLine1.Location = new System.Drawing.Point(151, 148);
+            this.uiLine1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiLine1.Name = "uiLine1";
+            this.uiLine1.Size = new System.Drawing.Size(306, 10);
+            this.uiLine1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLine1.TabIndex = 3;
+            this.uiLine1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
             // 
             // pClose
             // 
@@ -68,112 +149,77 @@
             // 
             this.lbDiaryApp.Font = new System.Drawing.Font("Cascadia Mono", 28.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
             this.lbDiaryApp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
-            this.lbDiaryApp.Location = new System.Drawing.Point(432, 54);
+            this.lbDiaryApp.Location = new System.Drawing.Point(0, 0);
             this.lbDiaryApp.Name = "lbDiaryApp";
-            this.lbDiaryApp.Size = new System.Drawing.Size(561, 70);
+            this.lbDiaryApp.Size = new System.Drawing.Size(572, 70);
             this.lbDiaryApp.TabIndex = 2;
             this.lbDiaryApp.Text = "DiaryApp";
-            this.lbDiaryApp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbDiaryApp.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // uiLine1
+            // uiTabControl1
             // 
-            this.uiLine1.FillColor = System.Drawing.Color.Transparent;
-            this.uiLine1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.uiLine1.LineCapSize = 10;
-            this.uiLine1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
-            this.uiLine1.LineColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
-            this.uiLine1.LineDashStyle = Sunny.UI.UILineDashStyle.Solid;
-            this.uiLine1.LineSize = 2;
-            this.uiLine1.Location = new System.Drawing.Point(589, 246);
-            this.uiLine1.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiLine1.Name = "uiLine1";
-            this.uiLine1.Size = new System.Drawing.Size(306, 10);
-            this.uiLine1.Style = Sunny.UI.UIStyle.Custom;
-            this.uiLine1.TabIndex = 3;
-            this.uiLine1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.uiTabControl1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.uiTabControl1.Controls.Add(this.tabPage1);
+            this.uiTabControl1.Controls.Add(this.tabPage2);
+            this.uiTabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.uiTabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.uiTabControl1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.uiTabControl1.Frame = null;
+            this.uiTabControl1.ItemSize = new System.Drawing.Size(150, 40);
+            this.uiTabControl1.Location = new System.Drawing.Point(426, 41);
+            this.uiTabControl1.MainPage = "";
+            this.uiTabControl1.Name = "uiTabControl1";
+            this.uiTabControl1.SelectedIndex = 0;
+            this.uiTabControl1.Size = new System.Drawing.Size(572, 528);
+            this.uiTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.uiTabControl1.TabIndex = 3;
+            this.uiTabControl1.TabUnSelectedForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.uiTabControl1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
             // 
-            // txtUserName
+            // tabPage1
             // 
-            this.txtUserName.BackColor = System.Drawing.SystemColors.Control;
-            this.txtUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtUserName.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtUserName.Location = new System.Drawing.Point(589, 215);
-            this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(306, 29);
-            this.txtUserName.TabIndex = 4;
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.pLock);
+            this.tabPage1.Controls.Add(this.lbDiaryApp);
+            this.tabPage1.Controls.Add(this.txtUserName);
+            this.tabPage1.Controls.Add(this.uiLine2);
+            this.tabPage1.Controls.Add(this.uiLine1);
+            this.tabPage1.Controls.Add(this.pUser);
+            this.tabPage1.Controls.Add(this.txtPass);
+            this.tabPage1.Location = new System.Drawing.Point(0, 0);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(572, 488);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
             // 
-            // pUser
+            // tabPage2
             // 
-            this.pUser.BackColor = System.Drawing.Color.Transparent;
-            this.pUser.Image = global::DiaryApp.Properties.Resources.user_green;
-            this.pUser.Location = new System.Drawing.Point(529, 206);
-            this.pUser.Name = "pUser";
-            this.pUser.Size = new System.Drawing.Size(40, 40);
-            this.pUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pUser.TabIndex = 5;
-            this.pUser.TabStop = false;
-            // 
-            // uiLine2
-            // 
-            this.uiLine2.FillColor = System.Drawing.Color.Transparent;
-            this.uiLine2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.uiLine2.LineCapSize = 10;
-            this.uiLine2.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
-            this.uiLine2.LineColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
-            this.uiLine2.LineDashStyle = Sunny.UI.UILineDashStyle.Solid;
-            this.uiLine2.LineSize = 2;
-            this.uiLine2.Location = new System.Drawing.Point(589, 338);
-            this.uiLine2.MinimumSize = new System.Drawing.Size(1, 1);
-            this.uiLine2.Name = "uiLine2";
-            this.uiLine2.Size = new System.Drawing.Size(306, 10);
-            this.uiLine2.Style = Sunny.UI.UIStyle.Custom;
-            this.uiLine2.TabIndex = 3;
-            this.uiLine2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
-            // 
-            // txtPass
-            // 
-            this.txtPass.BackColor = System.Drawing.SystemColors.Control;
-            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtPass.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtPass.Location = new System.Drawing.Point(589, 307);
-            this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(306, 29);
-            this.txtPass.TabIndex = 4;
-            // 
-            // pLock
-            // 
-            this.pLock.BackColor = System.Drawing.Color.Transparent;
-            this.pLock.Image = global::DiaryApp.Properties.Resources._lock;
-            this.pLock.Location = new System.Drawing.Point(529, 298);
-            this.pLock.Name = "pLock";
-            this.pLock.Size = new System.Drawing.Size(40, 40);
-            this.pLock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pLock.TabIndex = 5;
-            this.pLock.TabStop = false;
+            this.tabPage2.Location = new System.Drawing.Point(0, 0);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(200, 60);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(998, 569);
-            this.Controls.Add(this.pLock);
-            this.Controls.Add(this.pUser);
-            this.Controls.Add(this.txtPass);
-            this.Controls.Add(this.uiLine2);
-            this.Controls.Add(this.txtUserName);
-            this.Controls.Add(this.uiLine1);
-            this.Controls.Add(this.lbDiaryApp);
+            this.Controls.Add(this.uiTabControl1);
             this.Controls.Add(this.pClose);
             this.Controls.Add(this.pnImage);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            ((System.ComponentModel.ISupportInitialize)(this.pClose)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pUser)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pLock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pClose)).EndInit();
+            this.uiTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -188,5 +234,8 @@
         private Sunny.UI.UILine uiLine2;
         private TextBox txtPass;
         private PictureBox pLock;
+        private Sunny.UI.UITabControl uiTabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
     }
 }
