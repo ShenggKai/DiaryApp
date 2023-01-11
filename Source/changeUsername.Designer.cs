6bb1e156c6caca01284f5ae6b361e08b4544e83d
Sheng Kai <maimemdada@gmail.com>
2023-01-11 19:43:22 +0700

    done with ui change user

--- Source/changeUsername.Designer.cs
+++ Source/changeUsername.Designer.cs
@@ -29,22 +29,94 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(changeUsername));
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.uiLine1 = new Sunny.UI.UILine();
+            this.pUser = new System.Windows.Forms.PictureBox();
+            this.btnChangeUsername = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pUser)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BackColor = System.Drawing.SystemColors.Control;
+            this.txtUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUserName.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.txtUserName.Location = new System.Drawing.Point(93, 54);
+            this.txtUserName.MaxLength = 40;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.PlaceholderText = "Username";
+            this.txtUserName.Size = new System.Drawing.Size(350, 29);
+            this.txtUserName.TabIndex = 7;
+            // 
+            // uiLine1
+            // 
+            this.uiLine1.FillColor = System.Drawing.Color.Transparent;
+            this.uiLine1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.uiLine1.LineCapSize = 10;
+            this.uiLine1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
+            this.uiLine1.LineColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
+            this.uiLine1.LineDashStyle = Sunny.UI.UILineDashStyle.Solid;
+            this.uiLine1.LineSize = 2;
+            this.uiLine1.Location = new System.Drawing.Point(93, 89);
+            this.uiLine1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiLine1.Name = "uiLine1";
+            this.uiLine1.Size = new System.Drawing.Size(350, 10);
+            this.uiLine1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLine1.TabIndex = 6;
+            this.uiLine1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // pUser
+            // 
+            this.pUser.BackColor = System.Drawing.Color.Transparent;
+            this.pUser.Image = global::DiaryApp.Properties.Resources.user_green;
+            this.pUser.Location = new System.Drawing.Point(38, 45);
+            this.pUser.Name = "pUser";
+            this.pUser.Size = new System.Drawing.Size(40, 40);
+            this.pUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pUser.TabIndex = 8;
+            this.pUser.TabStop = false;
+            // 
+            // btnChangeUsername
+            // 
+            this.btnChangeUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(130)))), ((int)(((byte)(95)))));
+            this.btnChangeUsername.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnChangeUsername.FlatAppearance.BorderSize = 0;
+            this.btnChangeUsername.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChangeUsername.Font = new System.Drawing.Font("Segoe UI Semibold", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnChangeUsername.ForeColor = System.Drawing.Color.White;
+            this.btnChangeUsername.Location = new System.Drawing.Point(146, 129);
+            this.btnChangeUsername.Name = "btnChangeUsername";
+            this.btnChangeUsername.Size = new System.Drawing.Size(180, 57);
+            this.btnChangeUsername.TabIndex = 9;
+            this.btnChangeUsername.Text = "Đổi username";
+            this.btnChangeUsername.UseVisualStyleBackColor = false;
             // 
             // changeUsername
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(482, 221);
+            this.Controls.Add(this.btnChangeUsername);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.uiLine1);
+            this.Controls.Add(this.pUser);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "changeUsername";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Thay đổi username";
+            ((System.ComponentModel.ISupportInitialize)(this.pUser)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox txtUserName;
+        private Sunny.UI.UILine uiLine1;
+        private PictureBox pUser;
+        private Button btnChangeUsername;
     }
 }
